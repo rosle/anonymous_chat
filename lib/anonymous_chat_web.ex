809adb27cf85eaf7a121b76cc9f4d6e5a3ea570b
Rossukhon Leagmongkol <rose.roko@gmail.com>
2021-05-16 15:16:04 +0700

    Setup Phoenix LiveView

--- lib/anonymous_chat_web.ex
+++ lib/anonymous_chat_web.ex
@@ -42,10 +42,28 @@ defmodule AnonymousChatWeb do
     end
   end
 
+  def live_view do
+    quote do
+      use Phoenix.LiveView,
+        layout: {AnonymousChatWeb.LayoutView, "live.html"}
+
+      unquote(view_helpers())
+    end
+  end
+
+  def live_component do
+    quote do
+      use Phoenix.LiveComponent
+
+      unquote(view_helpers())
+    end
+  end
+
   def router do
     quote do
       use Phoenix.Router
 
+      import Phoenix.LiveView.Router
       import Plug.Conn
       import Phoenix.Controller
     end
@@ -62,6 +80,9 @@ defmodule AnonymousChatWeb do
     quote do
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
+
+      # Import LiveView helpers (live_render, live_component, live_patch, etc)
+      import Phoenix.LiveView.Helpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
