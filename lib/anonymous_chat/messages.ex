2da8c3a97c651caf41985a697a05fb90b8d32b9b
Rossukhon Leagmongkol <rose.roko@gmail.com>
2021-05-16 16:29:13 +0700

    Show message after inserted

--- lib/anonymous_chat/messages.ex
+++ lib/anonymous_chat/messages.ex
@@ -1,10 +1,29 @@
 defmodule AnonymousChat.Messages do
+  import Ecto.Query
+
   alias AnonymousChat.Messages.Message
   alias AnonymousChat.Repo
+  alias Phoenix.PubSub
+
+  @recent_messages_limit 50
+
+  def list_recent_messages do
+    Message
+    |> order_by(desc: :inserted_at)
+    |> limit(@recent_messages_limit)
+    |> Repo.all()
+  end
 
   def create_message(msg_attrs) do
     %Message{}
     |> Message.create_changeset(msg_attrs)
     |> Repo.insert()
+    |> notify_subscribers(:message_created)
+  end
+
+  defp notify_subscribers(repo_result, event) do
+    PubSub.broadcast(AnonymousChat.PubSub, "chat_room", {event, repo_result})
+
+    repo_result
   end
 end
--- lib/anonymous_chat_web/live/chat_room_live.ex
+++ lib/anonymous_chat_web/live/chat_room_live.ex
@@ -1,7 +1,24 @@
 defmodule AnonymousChatWeb.ChatRoomLive do
   use AnonymousChatWeb, :live_view
 
+  alias AnonymousChat.Messages
+  alias AnonymousChat.Messages.Message
+  alias Phoenix.PubSub
+
   def mount(_, _, socket) do
-    {:ok, socket}
+    if connected?(socket), do: PubSub.subscribe(AnonymousChat.PubSub, "chat_room")
+
+    {:ok, socket, temporary_assigns: [messages: list_recent_messages()]}
+  end
+
+  def handle_info({:message_created, {:ok, %Message{} = message}}, socket) do
+    {:noreply, assign(socket, :messages, [message])}
+  end
+
+  def handle_info(_, socket), do: {:noreply, socket}
+
+  defp list_recent_messages do
+    Messages.list_recent_messages()
+    |> Enum.reverse()
   end
 end
